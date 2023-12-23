2fd899e95867505ba3e5e4933fbf3ea61623ceb3
Nícolas <nicolasdamotaarruda@hotmail.com>
2023-12-23 02:46:59 -0300

    Feat: Adicionando som de botão dos menus.

--- Assets/MenuControllerScript.cs
+++ Assets/MenuControllerScript.cs
@@ -20,13 +20,14 @@ public class MenuControllerScript : MonoBehaviour
     //funcao usada no botao de comecar
     public void StartButtonFunction()
     {
-        //SceneManager.LoadScene(1);
+        SoundController.GetInstance().PlaySound("btn_click", null);
         TransitionsController.GetInstance().LoadNextScene();
     }
 
     //funcao usada no botao de mostrar controles
     public void ControlsButtonFunction()
     {
+        SoundController.GetInstance().PlaySound("btn_click", null);
         MainMenu.SetActive(false);
         ControlsDisplay.SetActive(true);
     }
@@ -34,6 +35,7 @@ public class MenuControllerScript : MonoBehaviour
     //funcao usada no botao de mostrar creditos
     public void CreditsButtonFunction()
     {
+        SoundController.GetInstance().PlaySound("btn_click", null);
         MainMenu.SetActive(false);
         CreditsDisplay.SetActive(true);
     }
@@ -47,6 +49,7 @@ public class MenuControllerScript : MonoBehaviour
     //funcao usada no botao de retornar pro menu principal
     public void ReturnButtonFunction()
     {
+        SoundController.GetInstance().PlaySound("btn_click", null);
         MainMenu.SetActive(true);
         CreditsDisplay.SetActive(false);
         ControlsDisplay.SetActive(false);
--- Assets/Scripts/Interface/GameOver.cs
+++ Assets/Scripts/Interface/GameOver.cs
@@ -4,9 +4,11 @@ public class GameOver : MonoBehaviour {
 
     //Métodos que serão ativados depois de algum botão da tela de game over ser pressionado
     public void ReturnToMenu() {
+        SoundController.GetInstance().PlaySound("btn_click", null);
         TransitionsController.GetInstance().LoadNextScene();
     }
     public void RestartGame() {
+        SoundController.GetInstance().PlaySound("btn_click", null);
         TransitionsController.GetInstance().LoadMainScene();
     }
     public void QuitGame() {
